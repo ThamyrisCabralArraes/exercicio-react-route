335d2b7bc0ee953d099197a4c04567ccde316831
thamyris <thamyriscabral@hotmail.com>
2023-03-10 11:47:49 -0300

    feat: usado o rediect

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Users from './components/Users';
 
 import { Link } from 'react-router-dom';
+import StrictAccess from './components/StrictAccess';
 
 class App extends Component {
   render() {
@@ -13,11 +14,13 @@ class App extends Component {
       <BrowserRouter>
       <Link to={'/'}>Home</Link> <span> | </span>
       <Link to={'/about'}>About</Link> <span> | </span>
-      <Link to={'/users'}>Users</Link>
+      <Link to={'/users'}>Users</Link> <span> | </span>
+      <Link to={'/strict'}>Strict</Link>
       <Switch>
         <Route path={'/'} component={Home} exact />
         <Route path={'/about'} component={About} />
         <Route path={'/users/:id'} render={(props) => <Users {...props} greetingsMessage={'Good Morning'} />} />
+        <Route path={'/strict'} render={() => <StrictAccess user='joao' password='1234' /> } />
       </Switch>
       </BrowserRouter>
       
---
+++ src/components/StrictAccess.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class StrictAccess extends Component {
+
+  render() {
+    const {user, password} = this.props
+
+    if (user !== "joao" || password !== "1234") {
+      console.log('opa')
+      alert("Acess denied")
+      return <Redirect to="/" />
+    }
+
+    return (
+      <div>
+        <h2>strictacess</h2>
+
+      </div>
+    );
+  }
+}
+
+export default StrictAccess;
