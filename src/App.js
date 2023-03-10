726d5ee01e470953dd6b58d3a4255d11d90d514a
thamyris <thamyriscabral@hotmail.com>
2023-03-10 11:02:01 -0300

    feat: criado users para usuarios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
@@ -14,9 +14,11 @@ class App extends Component {
       <Link to={'/'}>Home</Link> <span> | </span>
       <Link to={'/about'}>About</Link> <span> | </span>
       <Link to={'/users'}>Users</Link>
-      <Route path={'/'} component={Home} exact />
-      <Route path={'/about'} component={About} />
-      <Route path={'/users'} component={Users} exact />
+      <Switch>
+        <Route path={'/'} component={Home} exact />
+        <Route path={'/about'} component={About} />
+        <Route path={'/users/:id'} render={(props) => <Users {...props} greetingsMessage={'Good Morning'} />} />
+      </Switch>
       </BrowserRouter>
       
     );
