c8e6351366828dd46e5aaee2c0ac188654fedda2
thamyris <thamyriscabral@hotmail.com>
2023-03-10 11:21:09 -0300

    feat: criado um button que redireciona para o about

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   render() {
+    const {history} = this.props
     return (
       <div>
         <h2>Home</h2>
         <p> My awesome Home component </p>
+        <button 
+        type='button'
+        onClick={ () => history.push('/about') }
+        > ir para about</button>
       </div>
     );
   }
